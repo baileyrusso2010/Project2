7bd8d0bae657ed3a089156466dd3c9e04c8f5a1f
baileyrusso2010 <41842774+baileyrusso2010@users.noreply.github.com>
2020-10-20 00:35:27 -0400

    Init
    First Push

---
+++ Project2/index.js
@@ -0,0 +1,20 @@
+let canvas, ctx;
+
+window.onload = init;
+
+function init(){
+
+  canvas = document.querySelector("#myCanvas");
+  ctx = canvas.getContext('2d');
+
+  canvas.height = 500;
+  canvas.width = 500;
+
+
+  
+}//end of init
+
+function loop(){
+
+
+}//end of loop
